5237a985200b8b1a140d91711c49244f72a3da39
Abhijit Bhangale <abhijitbhangale3@gmail.com>
2024-02-07 23:18:03 +0530

    Modified the MyBlogs component  and mapped the data dynamically by fetching from mySQL database.

--- Travel_log/safar-ui/src/Auth/Login.js
+++ Travel_log/safar-ui/src/Auth/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Login = () => {
+ 
   return (
     <div className='container d-flex'>
       <form>
@@ -13,11 +14,11 @@ const Login = () => {
         </div>
         <div className="form-group">
           <label htmlFor="exampleInputEmail1">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+          <input type="email" className="form-control" required id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
         </div>
         <div className="form-group">
           <label htmlFor="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+          <input type="password" className="form-control" required id="exampleInputPassword1" placeholder="Password" />
         </div>
         <div className="form-check">
           <input type="checkbox" className="form-check-input" id="exampleCheck1" />
